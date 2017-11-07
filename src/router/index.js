96b934ed771b12495b1af56bdbc33c3b1921b0d5
gaiyulong <gyl@acewill.cn>
2017-11-07 18:56:51 +0800

    home.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Home from '@/page/home/home'
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
+	{
+      path: '/order',
+      name: 'order',
+      component: Home,
+    },
+	{
+      path: '/hello',
       name: 'Hello',
       component: HelloWorld,
     },
